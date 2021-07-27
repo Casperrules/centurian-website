ea855456aa998b4b498aff2b8f417fee4a4cd1cc
Adarsh Dubey <adarsh.dubey64@gmail.com>
2021-07-27 16:19:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function sendMessage(){
+    const subject = document.getElementById("subject").value;
+    const name  = document.getElementById("name").value;
+    const msg   = document.getElementById("message").value.toString();
+    if(name && msg && subject){
+        window.open('mailto:hello@centurion.co.in?subject='+subject+"-"+name+'&body='+msg);
+    }
+
+}
+function closePop(){
+    const div1 = document.getElementById("Attigo-popup");
+    const div2 = document.getElementById("IMS-popup");
+    console.log(div1.style.visibility)
+    console.log(div2.style.visibility)
+    div1.style.visibility = 'hidden';
+    div2.style.visibility = 'hidden';
+
+}
+function openAttigo(){
+    document.getElementById("Attigo-popup").style.visibility = 'visible';
+}
+function openIMS(){
+    document.getElementById("IMS-popup").style.visibility = 'visible';
+}
